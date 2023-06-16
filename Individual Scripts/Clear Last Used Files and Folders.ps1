5b4077d796f54d7a10414245e7ecf207eb735a2d
CodeNexus164 <110089537+CodeNexus164@users.noreply.github.com>
2023-06-16 16:28:40 +0100

    Optimize and refactor file removal PowerShell script
    This commit introduces several enhancements to the PowerShell script which removes the .automaticDestinations-ms files:
    
    1. Replaces `%APPDATA%` with `$env:APPDATA` for compatibility with PowerShell.
    
    2. Adds quotes around file paths to handle potential issues with special characters and spaces.
    
    3. Includes a `Test-Path` before removing the item to avoid potential errors when the directory or files do not exist.
    
    4. Uses the `-Recurse` switch with `Remove-Item` to ensure removal of all child items within the directory.
    
    5. Replaces `Write-Host` with `Write-Output` for better script maintainability.
    
    6. Adds meaningful comments for improved script readability.
    
    These changes make the script more robust, maintainable, and user-friendly by providing more descriptive output messages.

--- Individual Scripts/Clear Last Used Files and Folders.ps1
+++ Individual Scripts/Clear Last Used Files and Folders.ps1
@@ -1,2 +1,14 @@
-Write-Host "Clear last used files and folders"
-	Remove-Item %APPDATA%\Microsoft\Windows\Recent\AutomaticDestinations\*.automaticDestinations-ms -FORCE -ErrorAction SilentlyContinue
+# Output to the console
+Write-Output "Clear last used files and folders"
+
+# Define the target path
+$targetPath = "$env:APPDATA\Microsoft\Windows\Recent\AutomaticDestinations\*.automaticDestinations-ms"
+
+# Check if the target path exists
+if (Test-Path -Path $targetPath) {
+    # If the path exists, remove the item
+    Remove-Item -Path $targetPath -Force -Recurse -ErrorAction SilentlyContinue
+} else {
+    # If the path does not exist, output a message
+    Write-Output "The path $targetPath does not exist."
+}
